154a9fd5a9f154377c54807851574ecee42b18a1
Tenzin Chhosphel <tenchhos@gmail.com>
2016-11-18 11:31:21 -0500

    add alert for equality of cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,23 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardOne === cardTwo) {
+	alert("You found a match!")
+}
+else {
+	alert("Sorry, try again.")
+}
+
+if (cardThree === cardFour) {
+	alert("You found a match!")
+}
+else {
+	alert("Sorry, try again.")
+}
+
+if (cardTwo === cardFour) {
+	alert("You found a match!")
+}
+else {
+	alert("Sorry, try again.")
+}
