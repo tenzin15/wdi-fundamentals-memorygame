e0d364277ec3c34abf2e4caf36533aceea0b0ba4
Tenzin Chhosphel <tenchhos@gmail.com>
2016-11-18 13:33:56 -0500

    almost done - some errors with createBoard()

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,35 +1,10 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-/*
-if (cardOne === cardTwo) {
-	alert("You found a match!")
-}
-else {
-	alert("Sorry, try again.")
-}
-
-if (cardThree === cardFour) {
-	alert("You found a match!")
-}
-else {
-	alert("Sorry, try again.")
-}
-
-if (cardTwo === cardFour) {
-	alert("You found a match!")
-}
-else {
-	alert("Sorry, try again.")
-}
-*/
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
 function createCards(){
 	var gameBoard = document.getElementById('game-board');
-	console.log(gameBoard)
 	idx = 0;
 	while (idx < 4) {
 		var divElement = document.createElement('div');
@@ -39,4 +14,34 @@ function createCards(){
 	}
 }
 
-createCards();
+function isMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1])
+		alert("You found a match.")
+	else
+		alert("Sorry, try again.")
+}
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+function createBoard() {
+	var gameBoard = document.getElementById('game-board');
+	
+	for (var i = 0; i < cards.length; i++) {
+		
+		var divElement = document.createElement('div');
+		divElement.className = 'card';
+		gameBoard.appendChild(divElement);
+
+		cards[i].setAttribute('data-card', cards[i]);
+		cards[i].addEventListener('click', isTwoCards);
+	}
+}
+
+createBoard();
